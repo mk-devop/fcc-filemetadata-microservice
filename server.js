52ca8c979f32e199f88a81fc28e08ae2fbb24f86
mk-devop <101245598+mk-devop@users.noreply.github.com>
2022-03-10 15:08:19 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config()
+
+var app = express();
+
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+   // req.file is the name of your file in the form above, here 'uploaded_file'
+   // req.body will hold the text fields, if there were any 
+   //console.log(req.file, req.body)
+   res.json({ "name":req.file.originalname, "type":req.file.mimetype, "size":req.file.size});
+});
+
+//{"name":"Firefox Installer.exe","type":"application/x-msdownload","size":333984}
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
